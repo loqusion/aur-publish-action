c6f412df175c56373bd90fe4e26b2a8a8e75a831
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-14 14:04:23 -0500

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,35 +5,34 @@ set -euo pipefail
 PKGVER=${GITHUB_REF##*/v}
 REPO_URL="ssh://aur@aur.archlinux.org/${INPUT_PACKAGE_NAME}.git"
 
-echo "---------------- AUR Package version $INPUT_PACKAGE_NAME/$PKGVER ----------------"
-
+echo '::group::Configuring SSH'
 ssh-keyscan -t ed25519 aur.archlinux.org >>"$HOME"/.ssh/known_hosts
 echo -e "${INPUT_SSH_PRIVATE_KEY//_/\\n}" >"$HOME"/.ssh/aur
 chmod 600 "$HOME"/.ssh/aur*
+echo '::endgroup::'
 
+echo '::group::Configuring Git'
 git config --global user.name "$INPUT_COMMIT_USERNAME"
 git config --global user.email "$INPUT_COMMIT_EMAIL"
+echo '::endgroup::'
 
-echo "---------------- CLONING $REPO_URL ----------------"
+echo "::group::Cloning $REPO_URL into /tmp/aur-repo"
+git clone "$REPO_URL" "/tmp/aur-repo"
+cd "/tmp/aur-repo"
+echo '::endgroup::'
 
-git clone "$REPO_URL" "/tmp/$INPUT_PACKAGE_NAME"
-cd "/tmp/$INPUT_PACKAGE_NAME"
-
-echo "------------- BUILDING PKG $INPUT_PACKAGE_NAME ----------------"
-
+echo "::group::Building PKGBUILD for $INPUT_PACKAGE_NAME"
 sed -i "s/pkgver=.*$/pkgver=$PKGVER/" PKGBUILD
 sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
 perl -i -0pe "s/sha256sums=[\s\S][^\)]*\)/$(makepkg -g 2>/dev/null)/" PKGBUILD
-
 makepkg -c
 makepkg --printsrcinfo >.SRCINFO
+echo '::endgroup::'
 
-echo "------------- PUBLISHING ----------------"
-
+echo '::group::Pushing to AUR'
 git add PKGBUILD .SRCINFO
 git diff-index --exit-code --quiet HEAD || {
 	git commit -m "chore: bump version to $PKGVER"
 	git push && echo "::warning::Pushed to AUR"
 }
-
-echo "------------- SYNC DONE ----------------"
+echo '::endgroup::'
