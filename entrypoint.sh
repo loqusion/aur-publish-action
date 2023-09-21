5fd5f5e1a059b10af062f516529af30e3efb473e
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-20 19:53:11 -0500

    feat: add support for `workflow_dispatch`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -l
-
 set -euo pipefail
 
+export HOME=/home/builder
+
+# DEBUG
 ls -lA
 for x in "${!GITHUB_@}"; do
 	echo "$x" "${!x}"
 done
 
-export HOME=/home/builder
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
-VERSION=$(git -C "$GITHUB_WORKSPACE" describe --abbr=0 "$GITHUB_REF")
+
+VERSION=$GITHUB_REF
+if [[ $GITHUB_EVENT_NAME = "workflow_dispatch" ]]; then
+	echo "Attempting to resolve version from ref $GITHUB_REF"
+	git -C "$GITHUB_WORKSPACE" fetch --tags
+	VERSION=$(git -C "$GITHUB_WORKSPACE" describe --abbr=0 "$GITHUB_REF")
+fi
+
 PKGVER=${VERSION##*/v}
 HOST_URL="aur.archlinux.org"
 REPO_URL="ssh://aur@${HOST_URL}/${INPUT_PACKAGE_NAME}.git"
@@ -26,7 +34,6 @@ cp /ssh_config "$SSH_PATH/config"
 chmod +r "$SSH_PATH/config"
 eval "$(ssh-agent -s)"
 ssh-add "$SSH_PATH/aur"
-export GIT_SSH_COMMAND="ssh -i $SSH_PATH/aur -F $SSH_PATH/config -o UserKnownHostsFile=$SSH_PATH/known_hosts"
 echo '::endgroup::'
 
 echo '::group::Configuring Git'
@@ -35,7 +42,10 @@ git config --global user.email "$INPUT_GIT_EMAIL"
 echo '::endgroup::'
 
 echo "::group::Cloning $REPO_URL"
-git clone -v "$REPO_URL" "/tmp/aur-repo"
+(
+	export GIT_SSH_COMMAND="ssh -i $SSH_PATH/aur -F $SSH_PATH/config -o UserKnownHostsFile=$SSH_PATH/known_hosts"
+	git clone -v "$REPO_URL" "/tmp/aur-repo"
+)
 cd "/tmp/aur-repo"
 echo '::endgroup::'
 
