d2108a3733958cfec15c5f1933d951f93169b606
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-22 04:37:43 -0500

    Clean up unused script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,78 +1,100 @@
 #!/bin/bash -l
 set -euo pipefail
 
-# DEBUG
-chown root:root "$GITHUB_WORKSPACE"
-pwd
-ls -lA
-for x in "${!GITHUB_@}"; do
-	echo "$x" "${!x}"
-done
+USER=$(whoami)
+chown -R "${USER}:${USER}" "$GITHUB_WORKSPACE"
 
-VERSION=$GITHUB_REF
-if [[ $GITHUB_EVENT_NAME = "workflow_dispatch" ]]; then
-	echo "Attempting to resolve version from ref $GITHUB_REF"
-	git -C "$GITHUB_WORKSPACE" fetch --tags
-	git config --global --add safe.directory "$GITHUB_WORKSPACE"
-	VERSION=$(git -C "$GITHUB_WORKSPACE" describe --abbr=0 "$GITHUB_REF")
-fi
-
-PKGVER=${VERSION##*/v}
 HOST_URL="aur.archlinux.org"
 REPO_URL="ssh://aur@${HOST_URL}/${INPUT_PACKAGE_NAME}.git"
 
-echo '::group::Configuring SSH'
-export SSH_PATH="$HOME/.ssh"
-(
-	umask 077
-	mkdir -p "$SSH_PATH"
-	ssh-keyscan -t ed25519 "$HOST_URL" >>"$SSH_PATH/known_hosts"
-	echo -e "${INPUT_SSH_PRIVATE_KEY//_/\\n}" >"$SSH_PATH/aur"
-)
-cp /ssh_config "$SSH_PATH/config"
-chmod +r "$SSH_PATH/config"
-eval "$(ssh-agent -s)"
-ssh-add "$SSH_PATH/aur"
-echo '::endgroup::'
-
-echo '::group::Configuring Git'
-git config --global user.name "$INPUT_GIT_USERNAME"
-git config --global user.email "$INPUT_GIT_EMAIL"
-echo '::endgroup::'
-
-echo "::group::Cloning $REPO_URL"
-(
-	export GIT_SSH_COMMAND="ssh -i $SSH_PATH/aur -F $SSH_PATH/config -o UserKnownHostsFile=$SSH_PATH/known_hosts"
-	git clone -v "$REPO_URL" "/tmp/aur-repo"
-)
-cd "/tmp/aur-repo"
-echo '::endgroup::'
-
-echo "::group::Building PKGBUILD for $INPUT_PACKAGE_NAME $PKGVER"
-# Escape for sed
-PKGVER_SED=$(printf '%s\n' "$PKGVER" | sed -e 's/[\/&]/\\&/g')
-sed -i "s/pkgver=.*$/pkgver=$PKGVER_SED/" PKGBUILD
-sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
-sha256sums=$(sudo -u builder makepkg -g)
-perl -i -0pe "s/sha256sums=[\s\S][^\)]*\)/${sha256sums}/" PKGBUILD
-sudo -u builder makepkg -c
-# shellcheck disable=SC2024
-sudo -u builder makepkg --printsrcinfo >.SRCINFO
-echo '::endgroup::'
-
-echo '::group::PKGBUILD'
-cat PKGBUILD
-echo '::endgroup::'
-
-if [[ $INPUT_DEBUG == "true" ]]; then
-	echo 'DEBUG is enabled, exiting early.'
-	exit 0
-fi
-
-echo '::group::Pushing to AUR'
-git add PKGBUILD .SRCINFO
-git diff-index --exit-code --quiet HEAD || {
-	git commit -m "chore: bump version to $PKGVER"
-	git push && echo "::warning::Pushed to AUR"
+act_group() {
+	echo "::group::$1"
+	shift
+	"$@"
+	echo "::endgroup::"
 }
-echo '::endgroup::'
+
+act_group_start() {
+	echo "::group::$1"
+}
+
+act_group_end() {
+	echo "::endgroup::"
+}
+
+get_version() {
+	local version
+	if [[ $GITHUB_REF_TYPE = "tag" ]]; then
+		version=$GITHUB_REF
+	else
+		echo "Attempting to resolve version from ref $GITHUB_REF"
+		git -C "$GITHUB_WORKSPACE" fetch --tags --unshallow
+		version=$(git -C "$GITHUB_WORKSPACE" describe --abbr=0 "$GITHUB_REF")
+	fi >&2
+	echo "$version"
+}
+
+setup_ssh() {
+	export SSH_PATH="$HOME/.ssh"
+	# shellcheck disable=SC2174
+	mkdir -p -m 700 "$SSH_PATH"
+	ssh-keyscan -t ed25519 "$HOST_URL" >>"$SSH_PATH/known_hosts"
+	echo -e "${INPUT_SSH_PRIVATE_KEY//_/\\n}" >"$SSH_PATH/aur.key"
+	chmod 600 "$SSH_PATH/aur.key" "$SSH_PATH/known_hosts"
+	cp /ssh_config "$SSH_PATH/config"
+	chmod +r "$SSH_PATH/config"
+	eval "$(ssh-agent -s)"
+	ssh-add "$SSH_PATH/aur.key"
+}
+
+clone_aur_repo() {
+	# shellcheck disable=SC2030
+	(
+		export GIT_SSH_COMMAND="ssh -i $SSH_PATH/aur.key -F $SSH_PATH/config -o UserKnownHostsFile=$SSH_PATH/known_hosts"
+		git clone -v "$REPO_URL" "/tmp/aur-repo"
+		chown -R builder:builder "/tmp/aur-repo"
+	)
+	cd "/tmp/aur-repo"
+}
+
+update_pkgbuild() {
+	local pkgver_sed_escaped
+	# Escape for sed
+	pkgver_sed_escaped=$(printf '%s\n' "$PKGVER" | sed -e 's/[\/&]/\\&/g')
+	sed -i "s/pkgver=.*$/pkgver=$pkgver_sed_escaped/" PKGBUILD
+	sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
+	sudo -u builder updpkgsums
+	sudo -u builder makepkg --printsrcinfo | sudo -u builder tee .SRCINFO
+}
+
+build_pkg() {
+	sudo -u builder makepkg --syncdeps --cleanbuild --needed --noconfirm
+}
+
+push_to_aur() {
+	# shellcheck disable=SC2031
+	(
+		export GIT_SSH_COMMAND="ssh -i $SSH_PATH/aur.key -F $SSH_PATH/config -o UserKnownHostsFile=$SSH_PATH/known_hosts"
+		git add PKGBUILD .SRCINFO
+		if git diff-index --exit-code --quiet HEAD; then
+			echo "::warning::No changes to PKGBUILD or .SRCINFO"
+		else
+			git commit -m "chore: bump version to $PKGVER"
+			git push
+			echo "::warning::Pushed to AUR"
+		fi
+	)
+}
+
+act_group_start "Version"
+VERSION=$(get_version)
+PKGVER=${VERSION##*/v}
+echo "Version: $VERSION"
+act_group_end
+
+act_group 'Configure SSH' setup_ssh
+act_group "Clone AUR repository @ $REPO_URL" clone_aur_repo
+act_group "Update PKGBUILD for $INPUT_PACKAGE_NAME $PKGVER" update_pkgbuild
+act_group "Build package" build_pkg
+act_group "PKGBUILD" cat PKGBUILD
+# act_group "Push to AUR" push_to_aur
